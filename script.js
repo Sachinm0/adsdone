d77be409d36865f627987109bb88c217b233ec75
Sachin Magare <sachinm0345@gmail.com>
2025-05-06 11:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// Real or demo affiliate products
+// Products with offer % added
 const products = [
   {
-    link: "https://amzn.to/4jHP6eQ",
-    image: "https://m.media-amazon.com/images/I/713gRUj4l7L._SL1500_.jpg"
+    link: "https://amzn.to/44puEux",
+    image: "https://m.media-amazon.com/images/I/6186Xb7H1TL._SL1200_.jpg",
+    offer: "80% OFF"
   },
   {
-    link: "https://amzn.to/3GCqTIx",
-    image: "https://m.media-amazon.com/images/I/71nOmAy-OlL._SL1500_.jpg"
+    link: "https://amzn.to/3RPU4Ks",
+    image: "https://m.media-amazon.com/images/I/51C14VcBgyL._SL1500_.jpg",
+    offer: "46% OFF"
   },
   {
-    link: "https://amzn.to/3St5SlT",
-    image: "https://m.media-amazon.com/images/I/71grcs4BY-L._SY741_.jpg"
+    link: "https://amzn.to/4d7ILqL",
+    image: "https://m.media-amazon.com/images/I/61z8wnO6gSL._SX679_.jpg",
+    offer: "78% OFF"
   },
   {
-    link: "https://amzn.to/42O8G3m",
-    image: "https://m.media-amazon.com/images/I/51vSSbNMkfL._SX679_.jpg"
+    link: "https://amzn.to/43h7EwI",
+    image: "https://m.media-amazon.com/images/I/81Y+gtQJ1pL._SY679_.jpg",
+    offer: "79% OFF"
   },
   {
-    link: "https://amzn.to/4k4Wn8k",
-    image: "https://m.media-amazon.com/images/I/51z9ig1sQrL._SY625_.jpg"
+    link: "https://amzn.to/42LmvPZ",
+    image: "https://m.media-amazon.com/images/I/810LeYsd6pL._SX679_.jpg",
+    offer: "50% OFF"
   }
 ];
 
@@ -30,7 +35,7 @@ const shuffled = products
 
 const grid = document.getElementById("productGrid");
 
-// Insert promo blocks every 3 products
+// Insert promo boxes every 3 products
 shuffled.forEach((product, index) => {
   if (index % 3 === 0 && index !== 0) {
     const promo = document.createElement("div");
@@ -43,6 +48,7 @@ shuffled.forEach((product, index) => {
   item.className = "grid-item";
   item.innerHTML = `
     <a href="${product.link}" target="_blank" rel="noopener">
+      <span class="offer-badge">${product.offer}</span>
       <img src="${product.image}" alt="Affiliate Product" />
     </a>
   `;
